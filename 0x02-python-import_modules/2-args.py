cc6712be8317a121925e01ecd69ecc149916e207
NyasimiPhilip <nyasimiphilip@gmail.com>
2023-06-09 03:25:59 +0300

    ALX

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    argvLen = (len(sys.argv)) - 1
+    if argvLen == 1:
+        print("{} argument:".format(argvLen))
+    else:
+        if argvLen == 0:
+            print("{} arguments.".format(argvLen))
+        else:
+            print("{} arguments:".format(argvLen))
+            for i in range(1, argvLen + 1):
+                print("{}: {}".format(i, sys.argv[i]))
