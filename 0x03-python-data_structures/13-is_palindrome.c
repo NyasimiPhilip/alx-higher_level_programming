0a96a9595aa824ff267cd1bebfa144dd6bebc339
nyasimiphilip@gmail.com <nyasimiphilip@gmail.com>
2023-06-13 21:18:52 +0300

    Update

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,42 @@
+#include "lists.h"
+/**
+ * is_palindrome - check if list is a palindrome
+ * @head: double pointer to head
+ * Return: int
+ */
+int is_palindrome(listint_t **head)
+{
+	listint_t *trav = *head;
+	int i = 0, len;
+	int a , b;
+	int *mylist;
+	int reversedList[100];
+	if (*head == NULL)
+	{
+		return (1);
+	}
+	mylist = malloc(sizeof(int *));
+	if (mylist == NULL)
+	{
+		return (0);
+	}
+	while (trav->next != NULL)
+	{
+		mylist[i] = trav->n;
+		i++;
+		trav = trav->next;
+	}
+	mylist[i] = trav->n;
+	len = i + 1;
+	for (a = len - 1, b = 0; a >= 0; a--, b++) {
+		reversedList[b] = mylist[a];
+	}
+	for (a = 0; a < len; a++)
+	{
+		if (mylist[a] != reversedList[a])
+		{
+			return (0);
+		}
+	}
+	return (1);	        
+}
