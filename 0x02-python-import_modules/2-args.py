a2cd31d0d7f0065e1829bc0f67cd1b2294f7d83c
nyasimiphilip@gmail.com <nyasimiphilip@gmail.com>
2023-06-14 10:47:15 +0300

    ALX

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
+import sys
+def main():
+    num_args = len(sys.argv)
+    print(f"{num_args} arguments:")
+    if num_args == 0:
+        print(".")
+        return
+    for i in range(1, num_args + 1):
+        print(i, ":", sys.argv[i - 1])
 if __name__ == "__main__":
-    import sys
-    argvLen = (len(sys.argv)) - 1
-    if argvLen == 1:
-        print("{} argument:".format(argvLen))
-    else:
-        if argvLen == 0:
-            print("{} arguments.".format(argvLen))
-        else:
-            print("{} arguments:".format(argvLen))
-            for i in range(1, argvLen + 1):
-                print("{}: {}".format(i, sys.argv[i]))
+    main()
