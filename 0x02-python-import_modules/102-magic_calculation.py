9a37e2285cbcb0abdfed0ea011b2a0cb6a5e9e14
NyasimiPhilip <117896206+NyasimiPhilip@users.noreply.github.com>
2023-06-14 23:01:13 +0300

    Update 102-magic_calculation.py

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,10 +1,20 @@
 #!/usr/bin/python3
+import magic_calculation_102
 def magic_calculation(a, b):
-    from magic_calculation_102 import add, sub
+    """
+    Calculates the magic number between a and b.
+    Args:
+    a: The first number.
+    b: The second number.
+    Returns:
+    The magic number.
+    """
+    add = magic_calculation_102.add
+    sub = magic_calculation_102.sub
     if a < b:
         c = add(a, b)
         for i in range(4, 6):
             c = add(c, i)
-            return (c)
-        else:
-            return (sub(a, b))
+            return c
+    else:
+        return sub(a, b)
