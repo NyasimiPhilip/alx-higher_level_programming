2061f6ade064ab6692ae7d6aa4e78006aa572e09
nyasimiphilip@gmail.com <nyasimiphilip@gmail.com>
2023-06-15 21:30:39 +0300

    Update

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,24 +1,33 @@
 #!/usr/bin/python3
 # 12-roman_to_int.py
-ROMAN_VALUES = {
-        'I': 1,
-        'V': 5,
-        'X': 10,
-        'L': 50,
-        'C': 100,
-        'D': 500,
-        'M': 1000
-        }
+def roman_value(char):
+    if char == 'I':
+        return 1
+    if char == 'V':
+        return 5
+    if char == 'X':
+        return 10
+    if char == 'L':
+        return 50
+    if char == 'C':
+        return 100
+    if char == 'D':
+        return 500
+    if char == 'M':
+        return 1000
+        return -1
 def roman_to_int(roman_string):
-    if not isinstance(roman_string, str):
-        return (None)
+    if (not roman_string) or \
+            type(roman_string) != str:
+                return None
     number = 0
     for i in range(len(roman_string)):
         if i + 1 < len(roman_string):
-            if ROMAN_VALUES[roman_string[i]] >= ROMAN_VALUES[roman_string[i + 1]]:
-                number += ROMAN_VALUES[roman_string[i]]
+            if roman_value(roman_string[i]) \
+                    >= roman_value(roman_string[i + 1]):
+                number += roman_value(roman_string[i])
             else:
-                number -= ROMAN_VALUES[roman_string[i]]
+                number -= roman_value(roman_string[i])
             else:
-                number += ROMAN_VALUES[roman_string[i]]
-                return (number)
+                number += roman_value(roman_string[i])
+    return number
