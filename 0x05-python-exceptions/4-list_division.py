b0a0ab8db724b995ddaac80a29d279dfd092d29d
nyasimiphilip@gmail.com <nyasimiphilip@gmail.com>
2023-06-27 00:10:35 +0300

    Update

---
+++ 0x05-python-exceptions/4-list_division.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+def list_division(my_list_1, my_list_2, list_length):
+    new_l = []
+    for i in range(list_length):
+        try:
+            quotient = my_list_1[i] / my_list_2[i]
+        except TypeError:
+            print("wrong type")
+            quotient = 0
+        except ZeroDivisionError:
+            print("division by 0")
+            quotient = 0
+        except IndexError:
+            print("out of range")
+            quoitient = 0
+        finally:
+            new_l.append(quotient)
+    return (new_l)
