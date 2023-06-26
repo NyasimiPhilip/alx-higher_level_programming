ea87889040ebf726311b70d23eb90394698654d5
nyasimiphilip@gmail.com <nyasimiphilip@gmail.com>
2023-06-26 21:27:11 +0300

    Update

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -8,5 +8,5 @@ def safe_print_list(my_list, x):
             count += 1
         except IndexError:
             break
-        print()
+    print() # Add a new line after printing the numbers
     return (count)
