4efca65a19d875fbd5f0a9bbc416aab3981185b3
nyasimiphilip@gmail.com <nyasimiphilip@gmail.com>
2023-06-27 14:17:23 +0300

    Update

---
+++ 0x05-python-exceptions/102-main.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+from magic_calculation import magic_calculation
+magic_calculation(1, 2)
+magic_calculation(1, 4)
+magic_calculation(4, 1)
+magic_calculation(3, 3)
+magic_calculation(1, 1)
