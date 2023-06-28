fef72c905fa36cc31b3a20ea9d94340a97323c6d
NyasimiPhilip <117896206+NyasimiPhilip@users.noreply.github.com>
2023-06-28 16:35:16 +0300

    Update 4-square.py

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,15 +1,11 @@
 #!/usr/bin/python3
 """Define a class Square."""
-
-
 class Square:
     """Represent a square."""
 
     def __init__(self, size=0):
-        """Initialize a new square.
-
-        Args:
-            size (int): The size of the new square.
+        """
+        Initialize a new square.
         """
         self.size = size
 
