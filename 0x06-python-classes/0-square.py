57906a96507a42488aeb54db9ff8e2e355ca5cf5
nyasimiphilip@gmail.com <nyasimiphilip@gmail.com>
2023-06-28 15:12:41 +0300

    Update

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""creating a new class"""
+
+
+class Square:
+    """
+    new class
+    """
+    pass
---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# 1-square.py
+"""Define a new Square class."""
+
+
+class Square:
+    """Represent a square."""
+
+    def __init__(self, size):
+        self.__size = size
