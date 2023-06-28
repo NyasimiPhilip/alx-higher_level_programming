8d9c415cd0846e04e4ee845d90d8f24c6ead4588
NyasimiPhilip <117896206+NyasimiPhilip@users.noreply.github.com>
2023-06-28 17:30:01 +0300

    Update 5-square.py

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -6,6 +6,7 @@ class Square:
     new Square class
     """
     def __init__(self, size):
+       """Initialize a new square."""
         self.__size = size
 
     @property
@@ -24,6 +25,7 @@ class Square:
             raise ValueError("size must be >= 0")
         self.__size = value
     def area(self):
+      """Return the area."""
       return self.__size ** 2
 
     def my_print(self):
