965ef9e001c28b83f316e3ee05d64bf7c57bb44e
NyasimiPhilip <117896206+NyasimiPhilip@users.noreply.github.com>
2023-06-28 17:33:17 +0300

    Update 5-square.py

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -10,10 +10,8 @@ class Square:
 
     @property
     def size(self):
-      """
-      function to retrieve value of size
-      """
-        return self.__size
+      """function to retrieve value of size"""
+        return (self.__size)
 
     @size.setter
     def size(self, value):
@@ -22,6 +20,7 @@ class Square:
         elif value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
+    
     def area(self):
       """Return the area."""
       return self.__size ** 2
