33cc1a1139ade78d07225a05cbf5096bce3467cb
NyasimiPhilip <117896206+NyasimiPhilip@users.noreply.github.com>
2023-06-28 16:02:40 +0300

    Update 3-square.py

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -3,6 +3,9 @@
 
 
 class Square:
+    """
+    new class
+    """
     def __init__(self, size=0):
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
@@ -10,5 +13,10 @@ class Square:
             raise ValueError("size must be >= 0")
         self.__size = size
     def area(self):
+        """
+        return area of square
+        Returns:
+        area of square
+        """
         return self.__size * self.__size
     
