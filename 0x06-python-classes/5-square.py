b1993df67e04664b58b30a90c4d772244fd053e9
NyasimiPhilip <117896206+NyasimiPhilip@users.noreply.github.com>
2023-06-28 17:26:23 +0300

    Create 5-square.py

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""creating a new class"""
+
+class Square:
+  """
+    new Square class
+    """
+    def __init__(self, size):
+        self.__size = size
+
+    @property
+    def size(self):
+      """
+      function to retrieve value of size
+      """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """function to set value of size"""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def my_print(self):
+      """function that prints square with # symbols"""
+        if self.__size == 0:
+            print("")
+        else:
+            for _ in range(self.__size):
+                print("#" * self.__size)
