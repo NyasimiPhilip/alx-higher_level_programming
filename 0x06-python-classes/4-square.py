6ffc5411e3d1fe428bd65256c87c6f8b4004ec89
NyasimiPhilip <117896206+NyasimiPhilip@users.noreply.github.com>
2023-06-28 16:31:22 +0300

    Update 4-square.py

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,15 +1,20 @@
 #!/usr/bin/python3
-
+"""creating a new class"""
 class Square:
+    """
+    new class
+    """
     def __init__(self, size=0):
         self.__size = size
 
     @property
     def size(self):
+         """function to retrieve value of size"""
         return self.__size
 
     @size.setter
     def size(self, value):
+         """function to retrieve value of size"""
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
         elif value < 0:
