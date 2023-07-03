d0976e4fd29c3012ce5346f62e4804605917a850
nyasimiphilip@gmail.com <nyasimiphilip@gmail.com>
2023-07-03 16:41:16 +0300

    Update

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+"""
+Defining an empty Rectangle class
+"""
+
+
+class Rectangle:
+    """Empty Class that represents a rectangle"""
+    pass
---
+++ 0x08-python-more_classes/2-rectangle.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+# 2-rectangle.py
+"""Defines a Rectangle class."""
+class Rectangle:
+    """Represent a rectangle."""
+    def __init__(self, width=0, height=0):
+        self.set_width(width)
+        self.set_height(height)
+
+    def get_width(self):
+        return self.__width
+
+    def set_width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("Width must be an integer")
+        if value < 0:
+            raise ValueError("Width must be a non-negative integer")
+        self.__width = value
+
+    def get_height(self):
+        return self.__height
+
+    def set_height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("Height must be an integer")
+        if value < 0:
+            raise ValueError("Height must be a non-negative integer")
+        self.__height = value
+
+    def area(self):
+        return self.get_width() * self.get_height()
+
+    def perimeter(self):
+        return (self.get_width() + self.get_height()) * 2
