ffe6a86e4c7043bc179031954b10d51f325cebe8
NyasimiPhilip <117896206+NyasimiPhilip@users.noreply.github.com>
2023-07-05 00:39:55 +0300

    Update 101-nqueens.py

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -4,98 +4,86 @@
 """Solves the N-queens puzzle."""
 import sys
 
-class NQueensSolver:
-    def __init__(self, n):
-        self.n = n
-        self.board = self.init_board(n)
-        self.solutions = []
+def is_safe(board, row, col):
+    """
+    Check if it is safe to place a queen at position (row, col) on the board.
+    """
+    # Check for queens in the same column
+    for i in range(row):
+        if board[i][col] == 1:
+            return False
 
-    def init_board(self, n):
-        board = []
-        [board.append([]) for i in range(n)]
-        [row.append(' ') for i in range(n) for row in board]
-        return board
+    # Check for queens in the upper left diagonal
+    i, j = row, col
+    while i >= 0 and j >= 0:
+        if board[i][j] == 1:
+            return False
+        i -= 1
+        j -= 1
 
-    def board_deepcopy(self, board):
-        if isinstance(board, list):
-            return list(map(self.board_deepcopy, board))
-        return board
+    # Check for queens in the upper right diagonal
+    i, j = row, col
+    while i >= 0 and j < len(board):
+        if board[i][j] == 1:
+            return False
+        i -= 1
+        j += 1
 
-    def get_solution(self, board):
-        solution = []
-        for r in range(len(board)):
-            for c in range(len(board)):
-                if board[r][c] == "Q":
-                    solution.append([r, c])
-                    break
-        return solution
+    return True
 
-    def xout(self, board, row, col):
-        for c in range(col + 1, len(board)):
-            board[row][c] = "x"
-        for c in range(col - 1, -1, -1):
-            board[row][c] = "x"
-        for r in range(row + 1, len(board)):
-            board[r][col] = "x"
-        for r in range(row - 1, -1, -1):
-            board[r][col] = "x"
-        c = col + 1
-        for r in range(row + 1, len(board)):
-            if c >= len(board):
-                break
-            board[r][c] = "x"
-            c += 1
-        c = col - 1
-        for r in range(row - 1, -1, -1):
-            if c < 0:
-                break
-            board[r][c]
-            c -= 1
-        c = col + 1
-        for r in range(row - 1, -1, -1):
-            if c >= len(board):
-                break
-            board[r][c] = "x"
-            c += 1
-        c = col - 1
-        for r in range(row + 1, len(board)):
-            if c < 0:
-                break
-            board[r][c] = "x"
-            c -= 1
+def solve_nqueens(n):
+    """
+    Solve the N Queens problem and print all solutions.
+    """
+    board = [[0 for _ in range(n)] for _ in range(n)]
+    solutions = []
 
-    def recursive_solve(self, board, row, queens):
-        if queens == self.n:
-            self.solutions.append(self.get_solution(board))
+    def backtrack(row):
+        if row == n:
+            # Found a valid solution, add it to the solutions list
+            solution = []
+            for i in range(n):
+                for j in range(n):
+                    if board[i][j] == 1:
+                        solution.append([i, j])
+            solutions.append(solution)
             return
 
-        for col in range(len(board)):
-            if board[row][col] == " ":
-                tmp_board = self.board_deepcopy(board)
-                tmp_board[row][col] = "Q"
-                self.xout(tmp_board, row, col)
-                self.recursive_solve(tmp_board, row + 1, queens + 1)
+        for col in range(n):
+            if is_safe(board, row, col):
+                # Place a queen at position (row, col)
+                board[row][col] = 1
+                backtrack(row + 1)
+                # Remove the queen from position (row, col)
+                board[row][col] = 0
 
-    def solve(self):
-        self.recursive_solve(self.board, 0, 0)
+    # Start the backtracking algorithm
+    backtrack(0)
 
-        return self.solutions
+    # Print the solutions
+    for solution in solutions:
+        print(solution)
 
-
+# Main program
 if __name__ == "__main__":
+    # Check the number of arguments
     if len(sys.argv) != 2:
         print("Usage: nqueens N")
         sys.exit(1)
-    if not sys.argv[1].isdigit():
+
+    try:
+        # Get the value of N from the command line argument
+        N = int(sys.argv[1])
+
+        # Check the value of N
+        if N < 4:
+            print("N must be at least 4")
+            sys.exit(1)
+
+        # Solve the N Queens problem
+        solve_nqueens(N)
+
+    except ValueError:
         print("N must be a number")
         sys.exit(1)
-    if int(sys.argv[1]) < 4:
-        print("N must be at least 4")
-        sys.exit(1)
 
-    n = int(sys.argv[1])
-    solver = NQueensSolver(n)
-    solutions = solver.solve()
-
-    for sol in solutions:
-        print(sol)
