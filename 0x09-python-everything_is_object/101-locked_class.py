bee36bd925e48f52be0b12d8167ec94eccdc5a62
nyasimiphilip@gmail.com <nyasimiphilip@gmail.com>
2023-07-05 17:07:31 +0300

    Update

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
+
+
 """Defines locked class
 """
+
+
 class LockedClass:
-    """
-    Prevent object instantiation of new LockedClass attribute
-    other thatn first_name"""
-    __slots__ = ["first_name"]
+    """LockedClass prevents the creation of new instance attributes."""
+
+    __slots__ = 'first_name'
