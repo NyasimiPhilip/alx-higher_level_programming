4320ec4ed8d0ec9b9932517d68262cb6383f2f3f
nyasimiphilip@gmail.com <nyasimiphilip@gmail.com>
2023-07-11 12:19:17 +0300

    Update

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" Function to validate if obj is an instance of a_class
+"""
+
+
+def validate_inheritance(obj, a_class):
+
+    """
+    Function to validate if obj is an instance of a_class
+    Args:
+    obj: The object to check
+    a_class: The class to compare against
+    Returns:
+    True if obj is an instance of a_class, False otherwise
+    """
+    if type(obj) is a_class:
+        return False
+    return isinstance(obj, a_class)
