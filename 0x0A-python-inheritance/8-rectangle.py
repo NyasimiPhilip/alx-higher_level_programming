c21c07e8490dd2a1281d08eaf895b4c83f06d77a
nyasimiphilip@gmail.com <nyasimiphilip@gmail.com>
2023-07-11 20:28:19 +0300

    Update

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -3,14 +3,12 @@
 
 BaseGeometry = import('7-base_geometry').BaseGeometry
 
+
 class Rectangle(BaseGeometry):
-
     """Represents a rectangle."""
-
 
     def __init__(self, width, height):
         """Constructor method for Rectangle."""
-
 
         self.integer_validator("width", width)
         self.integer_validator("height", height)
