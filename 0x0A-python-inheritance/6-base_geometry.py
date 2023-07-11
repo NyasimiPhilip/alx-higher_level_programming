d99371e7f70c61743c4b25eedd9c789d1ac1541b
NyasimiPhilip <117896206+NyasimiPhilip@users.noreply.github.com>
2023-07-11 19:43:22 +0300

    Update 6-base_geometry.py

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
-"""Geometry class"""
+"""class Geometry"""
 
 
 class BaseGeometry:
     """BaseGeometry class"""
 
     def area(self):
-        """Function that raises an exception"""
-
-        raise Exception("calculate_area() is not implemented")
+        """func that raises an exception"""
+        raise Exception("area() is not implemented")
