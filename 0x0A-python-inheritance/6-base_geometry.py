8687e4f6abe88886909e319b2e343d5c9e5ae70f
nyasimiphilip@gmail.com <nyasimiphilip@gmail.com>
2023-07-11 19:12:08 +0300

    Update

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Geometry class"""
+
+
+class BaseGeometry:
+    """BaseGeometry class"""
+
+    def area(self):
+        """Function that raises an exception"""
+
+        raise Exception("calculate_area() is not implemented")
