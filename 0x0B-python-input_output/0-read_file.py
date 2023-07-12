8641f5f01dc419c19c2e636f022cadea1bc99796
NyasimiPhilip <117896206+NyasimiPhilip@users.noreply.github.com>
2023-07-12 05:12:59 +0300

    Update 0-read_file.py

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,2 +1,13 @@
-0x0B. Python - Input/Output
-Python
+#!/usr/bin/python3
+"""function to read a text file"""
+
+
+def read_file(filename=""):
+    """Reads a text file and prints its content.
+    
+    Args:
+        filename (str): The name of the file to be read.
+        
+    """
+    with open(filename, encoding="utf-8") as file:
+        print(file.read(), end='')
