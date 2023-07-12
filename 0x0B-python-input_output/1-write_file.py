ec492dafe39b8c4f19d264541b773cf9697e2851
NyasimiPhilip <117896206+NyasimiPhilip@users.noreply.github.com>
2023-07-12 05:19:40 +0300

    Create 1-write_file.py

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""func to write text to file"""
+
+
+def write_file(filename="", text=""):
+    """
+    Writes text to a file.
+    
+    Args:
+        filename (str): The name of the file.
+        text (str): The text to be written.
+        
+    Returns:
+        int: The number of characters written.
+    """
+    with open(filename, "w", encoding="utf-8") as file:
+        chars_written = file.write(text)
+        return chars_written
