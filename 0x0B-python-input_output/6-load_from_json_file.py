79a6c168da055fea07a6683c0b4a7ee5f88201b7
NyasimiPhilip <117896206+NyasimiPhilip@users.noreply.github.com>
2023-07-12 05:51:22 +0300

    Create 6-load_from_json_file.py

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""importing json module"""
+import json
+
+"""Funciton too load text from json file"""
+
+
+def load_from_json_file(filename):
+    """Loads JSON data from a file.
+    
+    Args:
+        filename (str): The name of the file.
+        
+    Returns:
+        obj: The loaded JSON object.
+    """
+    with open(filename, encoding="utf-8") as file:
+        json_data = file.read()
+        return json.loads(json_data)
