f168387d6eed593a0eaaa988880b2283ddf5415c
NyasimiPhilip <117896206+NyasimiPhilip@users.noreply.github.com>
2023-07-12 05:54:38 +0300

    Create 7-add_item.py

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""script that adds all arguments to a Python list,
+and then save them to a file"""
+
+"""importing sys and os.path"""
+
+import sys
+import os.path
+from json_utils import save_to_json_file, load_from_json_file
+
+filename = "add_item.json"
+args_list = []
+
+# Check if the file exists
+if os.path.exists(filename):
+    # If the file is empty, initialize the list to an empty list
+    if os.path.getsize(filename) == 0:
+        args_list = []
+    else:
+        # Load the existing list from the file
+        args_list = load_from_json_file(filename)
+
+# Append command line arguments to the list
+args_list.extend(sys.argv[1:])
+
+# Save the updated list to the file
+save_to_json_file(args_list, filename)
