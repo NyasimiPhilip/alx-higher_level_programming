0e67df47c81431e7a9c36987b59559008fcfa88d
NyasimiPhilip <117896206+NyasimiPhilip@users.noreply.github.com>
2023-07-12 22:20:45 +0300

    Update 7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -8,20 +8,15 @@ import os.path
 
 saveToJsonFile = __import__('5-save_to_json_file').save_to_json_file
 loadFromJsonFile = __import__('6-load_from_json_file').load_from_json_file
+
 filename = "add_item.json"
 args_list = []
-
-# Check if the file exists
+# if filename exsits
 if os.path.exists(filename):
-    # If the file is empty, initialize the list to an empty list
     if os.path.getsize(filename) == 0:
         args_list = []
     else:
-        # Load the existing list from the file
-        args_list = load_from_json_file(filename)
-
-# Append command line arguments to the list
-args_list.extend(sys.argv[1:])
-
-# Save the updated list to the file
-save_to_json_file(args_list, filename)
+        args_list = loadFromJsonFile(filename)
+for i in range(1, len(sys.argv)):
+    args_list.append(sys.argv[i])
+saveToJsonFile(args_list, filename)
