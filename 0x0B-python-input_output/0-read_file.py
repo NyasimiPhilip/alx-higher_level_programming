866fc63ea6c368d5128e2993fd8bd9b87d4de2a7
nyasimiphilip@gmail.com <nyasimiphilip@gmail.com>
2023-07-12 05:15:04 +0300

    Update

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -4,10 +4,8 @@
 
 def read_file(filename=""):
     """Reads a text file and prints its content.
-    
     Args:
         filename (str): The name of the file to be read.
-        
-    """
+        """
     with open(filename, encoding="utf-8") as file:
         print(file.read(), end='')
