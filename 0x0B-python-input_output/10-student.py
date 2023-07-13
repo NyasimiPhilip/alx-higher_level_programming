7233417ad7c4a2529f460ce939b6d2c30e1d75ec
NyasimiPhilip <117896206+NyasimiPhilip@users.noreply.github.com>
2023-07-13 05:23:39 +0300

    Update 10-student.py

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -7,7 +7,7 @@ class Student:
     """Represents a student."""
 
     def __init__(self, first_name, last_name, age):
-        """Initialize a new Student instance.
+        """Initialize a new Student instance with the provided attributes.
 
         Args:
             first_name (str): The first name of the student.
@@ -18,10 +18,20 @@ class Student:
         self.last_name = last_name
         self.age = age
 
-    def to_json(self):
+    def to_json(self, attrs=None):
         """Get a dictionary representation of the Student.
 
+        If attrs is provided and is a list of strings, only the attributes
+        specified in the list will be included in the dictionary.
+
+        Args:
+            attrs (list): (Optional) A list of attribute names to include.
+
         Returns:
-            dict: A dictionary containing the attributes of the Student instance.
+            dict: A dictionary representation of the Student instance.
+                  If attrs is provided, it only includes the specified attributes.
+                  Otherwise, it includes all attributes of the instance.
         """
+        if isinstance(attrs, list) and all(isinstance(attr, str) for attr in attrs):
+            return {attr: getattr(self, attr) for attr in attrs if hasattr(self, attr)}
         return self.__dict__
