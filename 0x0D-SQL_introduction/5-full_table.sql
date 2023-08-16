3e16d506744ba85996e8d9456c2322a8d9487e63
NyasimiPhilip <nyasimiphilip@gmail.com>
2023-08-16 10:33:21 +0300

    Prints full description

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,11 +1,6 @@
-# Database name provided as the first argument
-db_name=$1
-
-# MySQL command to retrieve table information
-mysql_cmd="SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_DEFAULT
+--prints full description
+SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_DEFAULT
            FROM INFORMATION_SCHEMA.COLUMNS
            WHERE TABLE_SCHEMA = '$db_name'
            AND TABLE_NAME = 'first_table';"
 
-# Execute the MySQL command
-mysql -u <username> -p<password> -e "$mysql_cmd" $db_name
