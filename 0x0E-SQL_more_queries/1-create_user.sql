1d4e428b270cd572fde5f4a3262877a5111c9378
nyasimiphilip@gmail.com <nyasimiphilip@gmail.com>
2023-08-16 21:36:55 +0300

    Create new user

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,4 +1,9 @@
--- create a MYSQL user
-CREATE USER IF NOT EXISTS 'user_0d_1@localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1@localhost';
-FLUSH PRIVILEGES;
+-- Check if user exists
+SELECT 1 FROM mysql.user WHERE user = 'user_0d_1' LIMIT 1;
+
+-- If user does not exist, create it
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+
+-- Grant all privileges to the user
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
+
