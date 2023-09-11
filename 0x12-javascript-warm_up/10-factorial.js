64106085ba8c370343907e3fa47f9be90b1b7678
nyasimiphilip@gmail.com <nyasimiphilip@gmail.com>
2023-09-11 19:54:02 +0300

    Update

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const num = Number(process.argv[2]);
+const fact = factorial(num);
+console.log(fact);
+
+function factorial (n) {
+  if (isNaN(n) || n <= 1) {
+    return 1;
+  }
+  return (n * factorial(n - 1));
+}
