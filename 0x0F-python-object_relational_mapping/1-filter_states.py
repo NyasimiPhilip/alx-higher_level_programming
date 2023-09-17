34dc3f0a5a9a9950d2c2f2f8712f63697615fbce
nyasimiphilip@gmail.com <nyasimiphilip@gmail.com>
2023-09-17 12:22:34 +0300

    Filter states by N

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+This script lists all states with a name starting with 'N' (uppercase 'N') from a MySQL database.
+"""
+
+if __name__ == "__main__":
+    # Import necessary libraries
+    import sys
+    import MySQLdb
+    # Get the command-line arguments for database user, password, and database name
+    dbUser = sys.argv[1]
+    pswd = sys.argv[2]
+    dbName = sys.argv[3]
+
+    # Connect to the MySQL database
+    db = MySQLdb.connect(host='localhost', user=dbUser, passwd=pswd, db=dbName)
+
+    # Create a cursor object to interact with the database
+    cur = db.cursor()
+
+    # Execute a SQL query to retrieve states with names starting with 'N'
+    cur.execute("SELECT * FROM states WHERE name LIKE 'N%'")
+
+    # Fetch all rows that match the query
+    rows = cur.fetchall()
+
+    # Print the results
+    for row in rows:
+        print(row)
+
+    # Close the cursor and the database connection
+    cur.close()
+    db.close()
