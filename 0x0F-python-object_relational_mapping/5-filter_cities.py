821baf09f5f836d1ead2a95a7441d48ccdc3d916
NyasimiPhilip <117896206+NyasimiPhilip@users.noreply.github.com>
2023-09-17 17:26:11 +0300

    Update 5-filter_cities.py

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,21 +1,46 @@
 #!/usr/bin/python3
-"""script that takes in the name of a state as an
-argument and lists all cities of that state,"""
+"""
+This script takes the name of a state as an argument and lists all cities of that
+state, using the database `hbtn_0e_4_usa`.
+"""
+
+import MySQLdb as db
+from sys import argv
 
 if __name__ == "__main__":
-    import sys
-    import MySQLdb
+    """
+    Establish a connection to the MySQL database and retrieve cities
+    for the specified state.
+    """
 
-    dbUser = sys.argv[1]
-    pswd = sys.argv[2]
-    dbName = sys.argv[3]
-    stateName = sys.argv[4]
+    # Connect to the database
+    db_connect = db.connect(host="localhost", port=3306,
+                            user=argv[1], passwd=argv[2], db=argv[3])
 
-    """make a connection w our db"""
-    db = MySQLdb.connect(host='localhost', user=dbUser, passwd=pswd, db=dbName)
+    # Create a cursor to interact with the database
+    with db_connect.cursor() as db_cursor:
+        # Execute SQL query to select cities of the specified state
+        db_cursor.execute("""
+            SELECT
+                cities.id, cities.name
+            FROM
+                cities
+            JOIN
+                states
+            ON
+                cities.state_id = states.id
+            WHERE
+                states.name LIKE BINARY %(state_name)s
+            ORDER BY
+                cities.id ASC
+        """, {
+            'state_name': argv[4]
+        })
+        
+        # Fetch all rows selected by the query
+        rows_selected = db_cursor.fetchall()
 
-    cur = db.cursor()
-    cur.execute("SELECT id FROM states WHERE name=%s", stateName)
-
-    res = cur.fetchonce()
-    cur.execute("SELECT name FROM cities WHERE id=%s", )
+    # Check if there are cities for the specified state
+    if rows_selected is not None:
+        # Print the names of the selected cities
+        print(", ".join([row[1] for row in rows_selected]))
