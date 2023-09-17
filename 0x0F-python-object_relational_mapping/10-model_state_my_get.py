ed5763853db60a19990abc6f92bcbae07a36e704
NyasimiPhilip <117896206+NyasimiPhilip@users.noreply.github.com>
2023-09-17 22:05:09 +0300

    Update 10-model_state_my_get.py

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,39 +1,44 @@
 #!/usr/bin/python3
 """
-This script prints the ID of the first State object
-from the database `hbtn_0e_6_usa` with a specified name.
+This script prints the ID of the State object with the name
+passed as an argument from the database hbtn_0e_6_usa.
 """
 
-from sys import argv
-from model_state import State
+import sys
 from sqlalchemy import create_engine
+from model_state import State, Base
 from sqlalchemy.orm import sessionmaker
 
 def main():
-    """
-    Access the database and retrieve the ID of the specified state from the database.
-    """
-    try:
-        # Database connection URL using f-strings
-        db_url = (
-            f"mysql+mysqldb://{argv[1]}:{argv[2]}@localhost:3306/{argv[3]}"
-        )        
-        # Create a database engine
-        engine = create_engine(db_url)        
-        # Create a session
-        Session = sessionmaker(bind=engine)
-        session = Session()
-        # Query and retrieve the ID of the specified state
-        state_name = argv[4]
-        state = session.query(State).filter(State.name == state_name).first()
-        if state:
-            print(f"{state.id}")
-        else:
-            print("Not found")
-        # Close the session
-        session.close()
-    except Exception as e:
-        print(f"An error occurred: {e}")
+    # Check if the correct number of command-line arguments is provided
+    if len(sys.argv) != 5:
+        print("Usage: python script.py <db_user> <db_password> <db_name> <state_name>")
+        sys.exit(1)
+    # Extract command-line arguments
+    db_user = sys.argv[1]
+    db_password = sys.argv[2]
+    db_name = sys.argv[3]
+    state_name = sys.argv[4]
+    # Create a database engine
+    engine = create_engine(
+        f"mysql://{db_user}:{db_password}@localhost:3306/{db_name}"
+    )
+    # Create a session
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    # Query and retrieve the State object with the specified name
+    result = (
+        session.query(State)
+        .filter(State.name == state_name)
+        .order_by(State.id.asc())
+        .all()
+    )
+    # Check if the result is not empty
+    if result:
+        for state in result:
+            print(state.id)
+    else:
+        print("Not found")
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
