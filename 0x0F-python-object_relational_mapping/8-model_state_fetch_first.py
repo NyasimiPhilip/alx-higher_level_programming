8a44cb211a216fe101a76512abf4948f090a3a31
NyasimiPhilip <117896206+NyasimiPhilip@users.noreply.github.com>
2023-09-17 20:23:11 +0300

    Update 8-model_state_fetch_first.py

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,39 +1,23 @@
 #!/usr/bin/python3
-"""
-This script prints the first State
-object from the database `hbtn_0e_6_usa`.
-"""
-from sys import argv
-from model_state import State
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-
-
-def main():
-    """
-    Access the database and retrieve the first state from the database.
-    """
-
-    try:
-        # Database connection URL using f-strings
-        db_url =
-        (f"mysql+mysqldb://{argv[1]}:{argv[2]}@localhost:3306/{argv[3]}")
-        # Create a database engine
-        engine = create_engine(db_url)
-        # Create a session
-        Session = sessionmaker(bind=engine)
-        session = Session()
-        # Query and print the first State object ordered by id
-        state = session.query(State).order_by(State.id).first()
-        if state:
-            print(f"{state.id}: {state.name}")
-        else:
-            print("No states found in the database.")
-            # Close the session
-            session.close()
-    except Exception as e:
-        print(f"An error occurred: {e}")
-
+"""script that prints the first State object from a database"""
 
 if __name__ == "__main__":
-    main()
+    import sys
+    from model_state import Base, State
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import sessionmaker
+
+    dbUser = sys.argv[1]
+    dbPwd = sys.argv[2]
+    dbName = sys.argv[3]
+
+    engine = create_engine(f"mysql://{dbUser}:{dbPwd}@localhost:3306/{dbName}")
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    res = session.query(State).order_by(State.id.asc()).first()
+
+    if res is not None:
+        print(f"{res.id}: {res.name}")
+    else:
+        print("Nothing")
