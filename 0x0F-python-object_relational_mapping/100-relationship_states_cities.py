d8672a3f64e2114f5e9e271785040d4b042341fc
nyasimiphilip@gmail.com <nyasimiphilip@gmail.com>
2023-09-19 15:33:28 +0300

    Update

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -1,52 +1,38 @@
 #!/usr/bin/python3
-"""
-This script prints all City objects
-from the database `hbtn_0e_14_usa`.
-"""
-
-from sys import argv
-from relationship_state import Base, State
-from relationship_city import City
+"""script that creates the State “California” with the City
+“San Francisco” from the database hbtn_0e_100_usa:"""
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from relationship_city import City
+from relationship_state import State
+from sqlalchemy.orm import declarative_base
 
+Base = declarative_base()
 
-def main():
-    """
-    The main function for accessing the database and creating City objects.
-    """
-    if len(argv) != 4:
-        print("Usage: python script.py <db_user> <db_password> <db_name>")
-        return
+if __name__ == '__main__':
 
-    db_user = argv[1]
-    db_password = argv[2]
-    db_name = argv[3]
+    import sys
+    from sqlalchemy import create_engine
+    from model_state import Base, State
+    from sqlalchemy.orm import sessionmaker
 
-    # Create the database URI
-    db_uri = f'mysql+mysqldb://{db_user}:{db_password}@' \
-        'localhost:3306/{db_name}'
-    # Create the engine and tables
-    engine = create_engine(db_uri)
+    dbUser = sys.argv[1]
+    dbPwd = sys.argv[2]
+    dbName = sys.argv[3]
+
+    # engine to connect to our db
+    engine = create_engine(f"mysql://{dbUser}:{dbPwd}@localhost:3306/{dbName}")
+
+    # create our tables
     Base.metadata.create_all(engine)
 
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    # Create a new State object for California
-    cal_state = State(name='California')
-
-    # Create a new City object for San Francisco
-    sfr_city = City(name='San Francisco')
-    cal_state.cities.append(sfr_city)
-
-    # Add the State and City objects to the session and commit the changes
-    session.add(cal_state)
+    new_state = State(name="California")
+    session.add(new_state)
     session.commit()
 
-    # Close the session
-    session.close()
-
-
-if __name__ == "__main__":
-    main()
+    new_city = City(name="Nairobi", state_id=new_state.id)
+    session.add(new_city)
+    session.commit()
