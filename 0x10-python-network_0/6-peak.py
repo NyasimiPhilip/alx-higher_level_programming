6d5a187fef7d54301c5dc0b6deb22dec27693d78
nyasimiphilip@gmail.com <nyasimiphilip@gmail.com>
2023-09-28 22:44:46 +0300

    Update

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+def find_peak(list_of_integers):
+    """
+    Find a peak in a list of unsorted integers using a divide and conquer approach.
+
+    Args:
+        list_of_integers (list): The list of integers to search for a peak in.
+
+    Returns:
+        int or None: The peak integer found in the list, or None if the list is empty.
+
+    Complexity:
+        The algorithm has a complexity of O(log(n)).
+
+    """
+    if not list_of_integers:
+        return None
+
+    left = 0
+    right = len(list_of_integers) - 1
+
+    while left < right:
+        mid = (left + right) // 2
+
+        if list_of_integers[mid] < list_of_integers[mid + 1]:
+            left = mid + 1
+        else:
+            right = mid
+
+    return list_of_integers[left]
