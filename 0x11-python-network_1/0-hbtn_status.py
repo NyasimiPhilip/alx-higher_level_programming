ae1d4d443b547ce9930998ec88818d8efb9e5480
nyasimiphilip@gmail.com <nyasimiphilip@gmail.com>
2023-10-17 15:13:06 +0300

    Update

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -10,9 +10,9 @@ def main():
     with request.urlopen(url) as response:
         body = response.read()
         print("Body response:")
-        print("\t- Type: {}".format(type(body)))
-        print("\t- Content: {}".format(body))
-        print("\t- UTF-8 Content: {}".format(body.decode("utf-8")))
+        print("\t- type: {}".format(type(body)))
+        print("\t- content: {}".format(body))
+        print("\t- utf8 content: {}".format(body.decode("utf-8")))
 
 
 if __name__ == "__main__":
